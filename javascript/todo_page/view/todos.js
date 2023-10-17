b35ba4f10b9774bbbd905132029b3fda7e1d6426
IMHOJEONG <39ghwjd@naver.com>
2023-10-18 00:55:33 +0900

    [add, revise]: add template.js & revise todos

--- javascript/todo_page/view/todos.js
+++ javascript/todo_page/view/todos.js
@@ -13,43 +13,76 @@ const createNewTodoNode = () => {
 }
 
 
-const getTodoElement = (todo) => {
+const getTodoElement = (todo, index, events) => {
     const { text, completed } = todo;
+
+    const element = createNewTodoNode();
+
+    element.querySelector('input.edit').value = text;
+    element.querySelector('label').textContext = text;
+
+    if(completed) {
+        element.classList.add('completed');
+        element 
+            .querySelector('input.toggle')
+            .checked = true;
+    }
+
+    const handler = e => events.deleteItem(index);
+
+    element    
+        .querySelector('button.destroy')
+        .addEventListener('click', handler);
+
+    return element;
+
   
-    return <template>
-<li>
-              <div class="view">
-                  <input 
-                      class="toggle"
-                      type="checkbox"
-                  />
-                  <label></label>
-                  <button class="destroy"></button>
-              </div>
-              <input class="edit" value="${text}" />
-          </li>
-        </template>
+//     return <template>
+// <li>
+//               <div class="view">
+//                   <input 
+//                       class="toggle"
+//                       type="checkbox"
+//                   />
+//                   <label></label>
+//                   <button class="destroy"></button>
+//               </div>
+//               <input class="edit" value="${text}" />
+//           </li>
+//         </template>
     
-    `
-          <li ${completed ? 'class="completed"' : ""}>
-              <div class="view">
-                  <input 
-                      ${completed ? "checked" : ""}
-                      class="toggle"
-                      type="checkbox"
-                  />
-                  <label>${text}</label>
-                  <button class="destroy"></button>
-              </div>
-              <input class="edit" value="${text}" />
-          </li>
-      `;
-  };
+//     `
+//           <li ${completed ? 'class="completed"' : ""}>
+//               <div class="view">
+//                   <input 
+//                       ${completed ? "checked" : ""}
+//                       class="toggle"
+//                       type="checkbox"
+//                   />
+//                   <label>${text}</label>
+//                   <button class="destroy"></button>
+//               </div>
+//               <input class="edit" value="${text}" />
+//           </li>
+//       `;
+};
   
-  export default (targetElement, { todos }) => {
+export default (targetElement, { todos }, events) => {
+
     const newTodoList = targetElement.cloneNode(true);
-    const todosElements = todos.map(getTodoElement).join("");
-    newTodoList.innerHTML = todosElements;
+    
+    newTodoList.innerHTML = '';
+
+    todos
+        .map((todo, index) => getTodoElement(todo, index, events))
+        .forEach(element => {
+            newTodoList.appendChild(element);
+        })
+
     return newTodoList;
-  };
+
+    // const todosElements = todos.map(getTodoElement).join("");
+    // newTodoList.innerHTML = todosElements;
+    // return newTodoList;
+};
   

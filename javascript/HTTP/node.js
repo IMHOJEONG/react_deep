143df3042b4a798bc13b46eb749e72ec9ff3e38c
IMHOJEONG <39ghwjd@naver.com>
2023-10-22 22:56:09 +0900

    [add]: add REST sample code

--- javascript/HTTP/node.js
+++ javascript/HTTP/node.js
@@ -1,19 +1,51 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {v4 : uuidv4 } = require('uuid');
-const findIndex = require('lodash.findIndex');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+const findIndex = require("lodash.findIndex");
 
-const http = require('node:http');
+const port = 8080;
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
+let todos = [];
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+app.use(bodyParser.json());
+
+app.get("/api/todos", (req, res) => {
+  res.send(todos);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+app.post("/api/todos", (req, res) => {
+  const newTodo = {
+    completed: false,
+    ...req.body,
+    id: uuidv4(),
+  };
+
+  todos.push(newTodo);
+
+  res.status(201);
+  res.send(newTodo);
+});
+
+app.patch("/api/todos/:id", (req, res) => {
+  const updateIndex = findIndex(todos, (t) => t.id === req.params.id);
+
+  const oldTodo = todos[updateIndex];
+
+  const newTodo = {
+    ...oldTodo,
+    ...req.body,
+  };
+
+  todos[updateIndex] = newTodo;
+
+  res.send(newTodo);
+});
+
+app.delete("/api/todos/:id", (req, res) => {
+  todos = todos.filter((t) => t.id !== req.params.id);
+  res.status(204);
+  res.send();
+});
+
+app.listen(port);
